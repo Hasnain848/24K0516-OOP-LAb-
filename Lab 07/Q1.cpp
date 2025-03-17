1792a7b94cea3b9e32b573d578d69288b9272cbe
Hasnain848 <muhammadhasnainsiddiqui596@gmail.com>
2025-03-17 22:30:19 +0500

    Update Q1.cpp

--- Lab 07/Q1.cpp
+++ Lab 07/Q1.cpp
@@ -1,105 +1,165 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Account{
-    protected:
+
+class Account
+{
+protected:
     int accountnumber;
     double balance;
     string accountname;
-    public:
-    Account(int accNum, double bal,string holder)
-        : accountnumber(accNum), balance(bal), accountname(holder) {}//, accountType(type)
-    void deposit(double amount){
-        if(amount<0){
-            cout<<"Invalid amount"<<endl;
+    string Accounttype;
+
+public:
+    Account(int accNum, double bal, string holder, string acctype)
+        : accountnumber(accNum), balance(bal), accountname(holder), Accounttype(acctype) {}
+
+    void deposit(double amount)
+    {
+        if (amount < 0)
+        {
+            cout << "Invalid amount" << endl;
             return;
         }
-        balance+=amount;
-         cout << "Amount: " << amount << " has been deposit to your Account" << endl;
-
+        balance += amount;
+        cout << "Amount: " << amount << " has been deposited into your account." << endl;
     }
+
     virtual void withdraw(double amount)
     {
         if (amount < 0)
         {
-            cout << "Invalid amount " << endl;
+            cout << "Invalid amount." << endl;
             return;
         }
         if (amount > balance)
         {
-            cout << "Sorry Insufficient balance" << endl;
+            cout << "Sorry, insufficient balance." << endl;
             return;
         }
         balance -= amount;
-        cout << "Amount: " << amount << " has been withdraw from you Account" << endl;
+        cout << "Amount: " << amount << " has been withdrawn from your account." << endl;
     }
-   virtual double Calculateinterest(){
 
-    }
-   virtual void printstatement(){
-    cout<<"=======Account Statement========"<<endl;
-    cout<<"Account number: "<<accountnumber<<endl;
-    cout<<"Balance: "<<balance<<endl;
-    }
-    void getinfo(){
-          cout<<"=======Account Details======="<<endl;
-    cout<<"Account number: "<<accountnumber<<endl;
-    cout<<"Name: "<<accountname<<endl;
-    cout<<"Balance: "<<balance<<endl;
-   
+    virtual double Calculateinterest() = 0;
 
+    virtual void printstatement()
+    {
+        cout << "========== Account Statement ==========" << endl;
+        cout << "Account number: " << accountnumber << endl;
+        cout << "Balance: " << balance << endl;
+    }
+
+    void getinfo()
+    {
+        cout << "========== Account Details ==========" << endl;
+        cout << "Account number: " << accountnumber << endl;
+        cout << "Name: " << accountname << endl;
+        cout << "Balance: " << balance << endl;
+        cout << "Account type: " << Accounttype << endl;
     }
 };
-class SavingsAccount : public Account{
+
+class SavingsAccount : public Account
+{
     float interestrate;
     double minimumbalance;
-    public:
-    double Calculateinterest()override{
-        return interestrate*balance;
-    };
-    void printstatement()override{
-         Account::printstatement();
-        cout<<"Interest rate: "<<interestrate<<endl;
-        cout<<"Minimum Balance"<<minimumbalance<<endl;
-        cout<<"Your interest: "<<Calculateinterest()<<endl;
 
+public:
+    SavingsAccount(int accNum, double bal, string holder, string acctype = "Saving", float rate = 2.0, double minbal = 2000)
+        : Account(accNum, bal, holder, acctype), interestrate(rate), minimumbalance(minbal) {}
+
+    double Calculateinterest() override
+    {
+        return (balance * interestrate) / 100;
     }
-    void withdraw(double amount)override{
-        if(amount>0 && (balance -amount)>=minimumbalance){
-            balance-=amount;
+
+    void printstatement() override
+    {
+        cout << "======= Savings Account Statement =======" << endl;
+        Account::printstatement();
+        cout << "Interest rate: " << interestrate << "%" << endl;
+        cout << "Minimum Balance: " << minimumbalance << endl;
+        cout << "Your interest: " << Calculateinterest() << endl;
+    }
+
+    void withdraw(double amount) override
+    {
+        if (amount > 0 && (balance - amount) >= minimumbalance)
+        {
+            balance -= amount;
+            cout << "Amount: " << amount << " has been withdrawn from your Savings Account." << endl;
         }
-        else cout<<"Error"<<endl;
-
+        else
+        {
+            cout << "Error: Insufficient balance or below minimum balance requirement." << endl;
+        }
     }
-
 };
-class CheckingAccount : public Account{
-    public:
-   double Calculateinterest()override{
 
-    };
-    void printstatement()override{
-       
+class CheckingAccount : public Account
+{
+public:
+    CheckingAccount(int accNum, double bal, string holder, string acctype = "Checking Account")
+        : Account(accNum, bal, holder, acctype) {}
 
+    double Calculateinterest() override { return 0; }
+
+    void printstatement() override
+    {
+        cout << "======= Checking Account Statement =======" << endl;
+        Account::printstatement();
     }
-
-
 };
-class FixeddepositAmount : public Account{
+
+class FixeddepositAmount : public Account
+{
     string maturitydate;
     float fixedinterestrate;
-    public:
-    double Calculateinterest()override{
-        return fixedinterestrate*balance;
 
+public:
+    FixeddepositAmount(int accNum, double bal, string holder, string acctype = "Fixed Deposit", string maturitydate = "", float fixrate = 3.0)
+        : Account(accNum, bal, holder, acctype), maturitydate(maturitydate), fixedinterestrate(fixrate) {}
 
-    };
-    void printstatement()override{
-        Account::printstatement();
-         cout<<"Interest rate: "<<fixedinterestrate<<endl;
-        cout<<" Maturity date"<<maturitydate<<endl;
-        cout<<"Your interest: "<<Calculateinterest()<<endl;
-
-        
+    double Calculateinterest() override
+    {
+        return (fixedinterestrate * balance) / 100;
     }
 
+    void printstatement() override
+    {
+        cout << "======= Fixed Deposit Account Statement =======" << endl;
+        Account::printstatement();
+        cout << "Interest rate: " << fixedinterestrate << "%" << endl;
+        cout << "Maturity date: " << maturitydate << endl;
+        cout << "Your interest: " << Calculateinterest() << endl;
+    }
+
+    void withdraw(double amount)
+    {
+        cout << "Withdrawal not allowed before maturity date." << endl;
+    }
 };
+
+int main()
+{
+    Account *a = new SavingsAccount(1234, 60000, "Hasnain", "Saving", 2.0, 2000);
+    a->getinfo();
+    a->deposit(3000);
+    a->withdraw(20000);
+    a->printstatement();
+    delete a;
+    cout << endl;
+
+    CheckingAccount obj(4321, 20000, "Yousha", "Checking");
+    obj.getinfo();
+    obj.deposit(3000);
+    obj.withdraw(4000);
+    obj.printstatement();
+    cout << endl;
+
+    FixeddepositAmount obj1(4321, 10000, "Obaid", "Fixed Deposit", "17-03-2025", 3.0);
+    obj1.getinfo();
+    obj1.deposit(4000);
+    obj1.withdraw(5000);
+    obj1.printstatement();
+}
