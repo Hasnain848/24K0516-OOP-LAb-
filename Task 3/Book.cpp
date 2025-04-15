87484a11221286657d06a605815580b1301b1530
Hasnain848 <muhammadhasnainsiddiqui596@gmail.com>
2025-04-15 19:59:50 +0500

    Delete Task 3 directory

--- Task 3/Book.cpp
+++
@@ -1,7 +0,0 @@
-#include "Book.h"
-#include <iostream>
-using namespace std;
-Book ::Book(string t, string a, string i) : title(t), author(a), isbn(i) {}
-string Book::gettitle() { return title; }
-string Book::getAuthor() { return author; }
-string Book::getISBN() { return isbn; }
