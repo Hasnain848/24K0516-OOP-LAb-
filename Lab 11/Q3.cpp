ad462f63fce8da4fb1a6eaaeac5f9dc5fbd2e0ab
Hasnain848 <muhammadhasnainsiddiqui596@gmail.com>
2025-04-30 20:56:30 +0500

    Update Q3.cpp

--- Lab 11/Q3.cpp
+++ Lab 11/Q3.cpp
@@ -1,44 +1,59 @@
 #include<iostream>
-//#include<exception>
 using namespace std;
-class FileException{
-    public:
-    virtual const char* what(){
+
+
+class FileException {
+public:
+    virtual const char* what() const{
         return "FileException occurred";
     }
+    virtual ~FileException() {}
 };
-class FileNotFound : public FileException{
-    public:
-    const char* what()override{
-    return("File not Exception");
+
+
+class FileNotFound : public FileException {
+public:
+    const char* what() const override {
+        return "FileNotFoundException - File not found!";
     }
 };
-class PermissionDenied : public FileException{
-    public:
-    const char* what()override{
-    return("Permission denied exception - Access Denied");
+
+
+class PermissionDenied : public FileException {
+public:
+    const char* what() const override {
+        return "PermissionDeniedException - Access denied!";
     }
 };
-void read(const string &filename){
-    if(filename == "missing.txt")
+
+
+void read(const string& filename) {
+    if (filename == "missing.txt")
         throw FileNotFound();
-    else if(filename =="secret.txt")
+    else if (filename == "secret.txt")
         throw PermissionDenied();
-    else  cout << "Reading '" << filename << "': File read successfully!" << endl;
+    else
+        cout << "Reading '" << filename << "': File read successfully!" << endl;
 }
-int main(){
-    string files[3]={"secret.txt","missing.txt","example.txt"};
-    for (int i=0;i<3;i++){
-        try{
+
+
+int main() {
+    string files[3] = { "secret.txt", "missing.txt", "example.txt" };
+
+    for (int i = 0; i < 3; i++) {
+        try {
             read(files[i]);
         }
-             catch (const FileNotFoundException& e) {
-            cout << "Reading '" << file << "': " << e.what() << endl;
+        catch (const FileNotFound& e) {
+            cout << "Reading '" << files[i] << "': " << e.what() << endl;
         }
-        catch (const PermissionDeniedException& e) {
-            cout << "Reading '" << file << "': " << e.what() << endl;
+        catch (const PermissionDenied& e) {
+            cout << "Reading '" << files[i] << "': " << e.what() << endl;
         }
         catch (const FileException& e) {
-            cout << "Reading '" << file << "': " << e.what() << endl;
+            cout << "Reading '" << files[i] << "': " << e.what() << endl;
         }
+    }
+
+    return 0;
 }
