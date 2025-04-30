abb0e26014fc9db5253e5bf4210ccc10293e1564
Hasnain848 <muhammadhasnainsiddiqui596@gmail.com>
2025-04-30 20:50:30 +0500

    Update Q3.cpp

--- Lab 11/Q3.cpp
+++ Lab 11/Q3.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 class FileException{
     public:
-    virtual const char* what()=0;
+    virtual const char* what(){
+        return "FileException occurred";
+    }
 };
 class FileNotFound : public FileException{
     public:
@@ -17,3 +19,26 @@ class PermissionDenied : public FileException{
     return("Permission denied exception - Access Denied");
     }
 };
+void read(const string &filename){
+    if(filename == "missing.txt")
+        throw FileNotFound();
+    else if(filename =="secret.txt")
+        throw PermissionDenied();
+    else  cout << "Reading '" << filename << "': File read successfully!" << endl;
+}
+int main(){
+    string files[3]={"secret.txt","missing.txt","example.txt"};
+    for (int i=0;i<3;i++){
+        try{
+            read(files[i]);
+        }
+             catch (const FileNotFoundException& e) {
+            cout << "Reading '" << file << "': " << e.what() << endl;
+        }
+        catch (const PermissionDeniedException& e) {
+            cout << "Reading '" << file << "': " << e.what() << endl;
+        }
+        catch (const FileException& e) {
+            cout << "Reading '" << file << "': " << e.what() << endl;
+        }
+}
